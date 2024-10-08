6b6ce44461e8b3c3f5683ec28a2eda3fab46e4a0
SvetoCopy <mrfairow@yandex.com>
2024-10-08 15:08:13 +0300

    PCA fix

--- PCA/src/PCA.cpp
+++ PCA/src/PCA.cpp
@@ -1,6 +1,7 @@
 #include "PCA.h"
 #include <algorithm>
 #include "assert.h"
+#include <iostream>
 
 bool PCACache::getFreePlace(int key, int index) {
 
@@ -11,6 +12,7 @@ bool PCACache::getFreePlace(int key, int index) {
 
     if (next_index < key_indexes[most_far.key].front()) {
         elems.erase(most_far.key);
+        size--;
         return true;
     }
     
@@ -33,6 +35,7 @@ void PCACache::updateMinMax() {
 
     int begin_elem_key = elems.begin()->second.key;
 
+    most_far = elems.begin()->second;
     int max_index = key_indexes[begin_elem_key].front();
 
     for (auto elem : elems) {
@@ -63,6 +66,7 @@ bool PCACache::put(int key, int value, int index) {
         
         if (place_available) {
             elems[key] = Node(key, value);
+            size++;
             updateMinMax();
         }
     } else if (get(key, index) != -1) return true;
